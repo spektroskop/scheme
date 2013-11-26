3c29fbae8c7823c095dfdf6948785d3f4c82944f
Alexander Stensrud <alex@stensrud.nu>
2013-11-26 20:55:39 +0100

    procedure arity

--- procedure.rb
+++ procedure.rb
@@ -7,7 +7,17 @@ end
 class Procedure
     attr_accessor :name
 
+    def arity(args)
+        num = args.length
+        if num < @required or (num > @required and not @rest)
+            arg = @rest.nil? ? @required : "at least #{@required}"
+            error "`#{@name}': wrong number of arguments:" +
+                  " #{num} given, #{arg} expected"
+        end
+    end
+
     def call(args, scope)
+        arity(args)
         args = args.array.map{|node| Scheme.evaluate(node, scope) }
         apply(args)
     end
@@ -21,6 +31,9 @@ end
 
 class Primitive < Procedure
     def initialize(&block)
+        @rest = block.arity < 0
+        @required = block.arity
+        @required = @required.next.abs if @rest
         @block = block
     end
 
