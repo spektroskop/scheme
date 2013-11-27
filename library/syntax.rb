387b49c7a7be61ed44aff70e5e900933cbc04c4e
Alexander Stensrud <alex@stensrud.nu>
2013-11-27 20:42:16 +0100

    more syntax

--- library/syntax.rb
+++ library/syntax.rb
@@ -5,6 +5,16 @@ def sequence(seq)
     seq
 end
 
+def args(nodes)
+    nodes.array.tap do |nodes|
+        return nodes.map(&:car), nodes.map(&:cadr)
+    end
+end
+
+syntax("load") do |scope, nodes|
+    scope.load(nodes.car)
+end
+
 syntax("define") do |scope, nodes|
     if nodes.car.symbol?
         expr = Scheme.evaluate(scope, nodes.cadr)
@@ -27,4 +37,37 @@ end
 
 syntax("quote") do |scope, nodes|
     nodes.car
+end
+
+syntax("if") do |scope, nodes|
+    Frame.new scope,
+        if Scheme.evaluate(scope, nodes.car)
+            nodes.cadr
+        elsif nodes.cddr.null?
+            Empty
+        else
+            nodes.caddr
+        end
+end
+
+syntax("let") do |scope, nodes|
+    Frame.new scope,
+        if Symbol === nodes.car
+            names, values = args(nodes.cadr)
+            List([:letrec, [[nodes.car,
+                 [:lambda, names, sequence(nodes.caddr)]]],
+                 [nodes.car, *values]])
+        else
+            names, values = args(nodes.car)
+            List([[:lambda, names,
+                 sequence(nodes.cdr)],
+                 *values])
+        end
+end
+
+syntax("letrec") do |scope, nodes|
+    Frame.new(scope,
+        List(nodes.car.reduce([:let, []]) { |expr, node|
+                expr.push([:define, node.car, node.cadr])
+             }.push(nodes.cadr)))
 end
