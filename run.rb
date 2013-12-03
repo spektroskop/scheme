56f8ca980b4b80e2a03ae1d71001e6767b0b7cb1
Alexander Stensrud <alex@stensrud.nu>
2013-12-03 20:39:01 +0100

    remove parse option

--- run.rb
+++ run.rb
@@ -1,17 +1,12 @@
 require "./scheme"
 
-parse = ARGV.include?("p")
-
 Scheme.setup
 
 begin
     Scheme.readline do |line|
-        if parse
-            result = Scheme.parse(line)
-        else
-            result = Scheme.run(nil, line)
+        Scheme.run(nil, line).tap do |x|
+            puts Paint["=> #{x}", :cyan, :bold]
         end
-        puts Paint["=> #{result}", :cyan, :bold]
     end
 rescue Scheme::Error => e
     puts Paint[e.message, :red, :bold]
